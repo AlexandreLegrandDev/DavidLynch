409f8c12cf53fe7303ef3457c94b8cc0410b9adc
Alexandre LEGRAND <legrand.alexandre777@gmail.com>
2026-02-11 11:03:42 +0100

    Ajout du projet au git

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const container = document.getElementById('scene-container');
+    const elements = document.querySelectorAll('.element');
+    const bg = document.querySelectorAll('.bg');
+
+    // Fonction pour lancer l'animation
+    const startAnimation = () => {
+        // On ajoute la classe 'active' au parent
+        // CSS s'occupe du reste des transitions
+        container.classList.add('active');
+        
+        console.log("Animation lancée : ouverture des rideaux");
+    };
+
+    // On ajoute l'écouteur de clic sur chaque élément du décor
+    elements.forEach(el => {
+        el.addEventListener('click', () => {
+            startAnimation();
+
+        });
+    });
+
+
+    // Optionnel : permettre de réinitialiser en cliquant sur le fond après 2s
+    container.addEventListener('dblclick', () => {
+        container.classList.remove('active');
+    });
+
+});
+
+
+function scaleScene() {
+  const scene = document.getElementById('scene');
+  const scale = Math.min(
+    window.innerWidth / 1920,
+    window.innerHeight / 1080
+  );
+  scene.style.transform = `scale(${scale})`;
+}
+
+window.addEventListener('resize', scaleScene);
+scaleScene();
